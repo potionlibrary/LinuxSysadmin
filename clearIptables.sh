59fa7008185bd298a74884aa720c5605e82a201d
Wagner, J <35028309+dota247@users.noreply.github.com>
2019-09-09 12:36:58 -0700

    Update clearIptables.sh

--- clearIptables.sh
+++ clearIptables.sh
@@ -1,1 +1,6 @@
-
+iptables --flush
+iptables --delete-chain
+iptables --table nat --flush
+iptables --table filter --flush
+iptables --table nat --delete-chain
+iptables --table filter --delete-chain
