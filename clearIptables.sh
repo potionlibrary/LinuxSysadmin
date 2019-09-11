deb0268c695169291463e43a3eacc16e21218077
Wagner, J <35028309+dota247@users.noreply.github.com>
2019-09-11 13:11:55 -0700

    Update clearIptables.sh

--- clearIptables.sh
+++ clearIptables.sh
@@ -9,6 +9,13 @@ iptables --table filter --flush
 iptables --table nat --delete-chain
 iptables --table filter --delete-chain
 
+ip6tables --flush
+ip6tables --delete-chain
+ip6tables --table nat --flush
+ip6tables --table filter --flush
+ip6tables --table nat --delete-chain
+ip6tables --table filter --delete-chain
+
 echo ""
 
 iptables -L -n
