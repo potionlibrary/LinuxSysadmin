4a8f41de1947b105d365e097ebab2bb24d52f9fe
Wagner, J <35028309+dota247@users.noreply.github.com>
2019-09-11 14:01:03 -0700

    Update setiptables.sh

--- setiptables.sh
+++ setiptables.sh
@@ -1,1 +1,6 @@
 
+sudo iptables -I INPUT 1 -i eth0 -p tcp --dport 8080 -j ACCEPT
+sudo iptables -I INPUT 1 -i eth0 -p tcp --dport 8443 -j ACCEPT
+
+sudo ip6tables -I INPUT 1 -i eth0 -p tcp --dport 8080 -j ACCEPT
+sudo ip6tables -I INPUT 1 -i eth0 -p tcp --dport 8443 -j ACCEPT
