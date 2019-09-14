1448e0fb73767c69c3889005344b7673ba47d1e1
Wagner, J <35028309+dota247@users.noreply.github.com>
2019-09-13 20:15:36 -0700

    Update apache-tomcat-install.sh

--- apache-tomcat-install.sh
+++ apache-tomcat-install.sh
@@ -28,36 +28,6 @@ sudo chgrp -R tomcat lib
 sudo chmod g+rwx bin
 sudo chmod g+r bin/*
 
-sudo vi /etc/systemd/system/tomcat.service
-
-======================
-set as
-
-[Unit]
-Description=Apache Tomcat Web Application Container
-After=syslog.target network.target
-
-[Service]
-Type=forking
-
-Environment=JAVA_HOME=/usr/lib/jvm/jre
-Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
-Environment=CATALINA_HOME=/opt/tomcat
-Environment=CATALINA_BASE=/opt/tomcat
-Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
-Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
-
-ExecStart=/opt/tomcat/bin/startup.sh
-ExecStop=/bin/kill -15 $MAINPID
-
-User=tomcat
-Group=tomcat
-
-[Install]
-WantedBy=multi-user.target
-
-==========================
-
 
 sudo iptables -I INPUT 1 -i eth0 -p tcp --dport 8080 -j ACCEPT
 
