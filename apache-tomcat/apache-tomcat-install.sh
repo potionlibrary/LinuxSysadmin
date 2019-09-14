8fc7afde0f7b83f8151756ea7bb7191b51be7f32
Wagner, J <35028309+dota247@users.noreply.github.com>
2019-09-13 20:12:40 -0700

    Update apache-tomcat-install.sh

--- apache-tomcat/apache-tomcat-install.sh
+++ apache-tomcat/apache-tomcat-install.sh
@@ -11,8 +11,6 @@ sudo groupadd tomcat
 sudo mkdir /opt/tomcat
 sudo useradd -s /bin/nologin -g tomcat -d /opt/tomcat tomcat
 
-
-
 cd ~
 wget https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.45/bin/apache-tomcat-8.5.45.tar.gz
 sudo tar -zxvf apache-tomcat-8.5.45.tar.gz -C /opt/tomcat --strip-components=1
@@ -31,36 +29,6 @@ sudo chmod g+r bin/*
 
 sudo vi /etc/systemd/system/tomcat.service
 
-======================
-set as
-
-[Unit]
-Description=Apache Tomcat Web Application Container
-After=syslog.target network.target
-
-[Service]
-Type=forking
-
-Environment=JAVA_HOME=/usr/lib/jvm/jre
-Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
-Environment=CATALINA_HOME=/opt/tomcat
-Environment=CATALINA_BASE=/opt/tomcat
-Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
-Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
-
-ExecStart=/opt/tomcat/bin/startup.sh
-ExecStop=/bin/kill -15 $MAINPID
-
-User=tomcat
-Group=tomcat
-
-[Install]
-WantedBy=multi-user.target
-
-==========================
-
-
 sudo iptables -I INPUT 1 -i eth0 -p tcp --dport 8080 -j ACCEPT
-
 
 sudo systemctl restart tomcat.service
