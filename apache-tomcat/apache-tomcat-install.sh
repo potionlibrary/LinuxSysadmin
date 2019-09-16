803f64f2f42ec8a8bf6f9fe3045261a90ec501c3
Wagner, J <35028309+dota247@users.noreply.github.com>
2019-09-16 13:47:08 -0700

    Update apache-tomcat-install.sh

--- apache-tomcat/apache-tomcat-install.sh
+++ apache-tomcat/apache-tomcat-install.sh
@@ -1,8 +1,8 @@
-yum update -y 
-yum install httpd -y 
-yum install wget -y 
-yum install iptables -y 
-yum install iptables-services -y 
+yum update -y
+yum install httpd -y
+yum install wget -y
+yum install iptables -y
+yum install iptables-services -y
 yum install java-1.8.0-openjdk.x86_64 -y
 
 sudo mkdir /opt/tomcat
@@ -11,7 +11,7 @@ cd ~
 wget https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.45/bin/apache-tomcat-8.5.45.tar.gz
 sudo tar -zxvf apache-tomcat-8.5.45.tar.gz -C /opt/tomcat --strip-components=1
 
-echo export CATALINA_HOME="/usr/tomcat" >> ~/.bashrc
+echo export CATALINA_HOME="/opt/tomcat" >> ~/.bashrc
 source ~/.bashrc
 
 cd /opt/tomcat/bin
