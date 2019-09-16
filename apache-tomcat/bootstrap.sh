433dd1b45ba723cdde19f96843db11e7dafe2961
Wagner, J <35028309+dota247@users.noreply.github.com>
2019-09-16 12:27:01 -0700

    Update bootstrap.sh

--- apache-tomcat/bootstrap.sh
+++ apache-tomcat/bootstrap.sh
@@ -1,9 +1,1 @@
-yum update -y
-
-yum install httpd -y 
-
-yum installl wget -y 
-
-yum install iptables -y
-
-yum install iptables-services -y
+yum update -y; yum install httpd -y; yum installl wget -y; yum install iptables -y; yum install iptables-services -y
