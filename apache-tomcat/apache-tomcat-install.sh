ac9086621b24a5d06ea382ad20103964a0341e43
Wagner, J <35028309+dota247@users.noreply.github.com>
2019-09-16 13:37:55 -0700

    Update apache-tomcat-install.sh

--- apache-tomcat/apache-tomcat-install.sh
+++ apache-tomcat/apache-tomcat-install.sh
@@ -5,27 +5,17 @@ yum install iptables -y
 yum install iptables-services -y 
 yum install java-1.8.0-openjdk.x86_64 -y
 
-
-sudo groupadd tomcat
 sudo mkdir /opt/tomcat
-sudo useradd -s /bin/nologin -g tomcat -d /opt/tomcat tomcat
 
 cd ~
 wget https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.45/bin/apache-tomcat-8.5.45.tar.gz
 sudo tar -zxvf apache-tomcat-8.5.45.tar.gz -C /opt/tomcat --strip-components=1
 
+echo "export CATALINA_HOME="/usr/tomcat"" >> ~/.bashrc
+source ~/.bashrc
 
-cd /opt/tomcat
-sudo chgrp -R tomcat conf
-sudo chmod g+rwx conf
-sudo chmod g+r conf/*
-sudo chown -R tomcat logs/ temp/ webapps/ work/
+cd /opt/tomcat/bin
 
-sudo chgrp -R tomcat bin
-sudo chgrp -R tomcat lib
-sudo chmod g+rwx bin
-sudo chmod g+r bin/*
+bash ./startup.sh
 
 sudo iptables -I INPUT 1 -i eth0 -p tcp --dport 8080 -j ACCEPT
-
-sudo systemctl restart tomcat.service
